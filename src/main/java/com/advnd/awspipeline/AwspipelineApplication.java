19db9f376aa09680f5a03b63cb844441373b8cbf
Sreelekshmiust <Sreelekshmi.Kumar@ust.com>
2023-06-02 16:42:47 +0530

    first commit

---
+++ src/main/java/com/advnd/awspipeline/AwspipelineApplication.java
@@ -0,0 +1,13 @@
+package com.advnd.awspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwspipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwspipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/advnd/awspipeline/AwspipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advnd.awspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwspipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
